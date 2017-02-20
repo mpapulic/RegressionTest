449cf139eef3c6a80bf516f013824efd674e1c72
Milan Papulic <milan.papulic@gmail.com>
2017-02-20 14:42:35 +0100

    Dodana jos dva testa na stranici Preference za testianje Certification of deed dela da li da se pojavljuje odmah ili tek pred Submit to CH

--- PluginLibrary/Helper/UsersProcessing.cs
+++ PluginLibrary/Helper/UsersProcessing.cs
@@ -14,15 +14,14 @@ namespace PluginLibrary.Models
             List<User> listUser = new List<User>();
 
 
-            System.IO.File.WriteAllText(@"C:\Temp\STEP1.txt", "Pred deserializaciju:" + textJSON + " a trazimo usera sa mail adresom :" + usermail);
+            System.IO.File.WriteAllText(@"C:\Temp\TestGetUserByMail.txt", "Usao u GetUserByMail:" + textJSON + " a trazimo usera sa mail adresom :" + usermail);
             try
             {
-                dynamic newJSON = JsonConvert.DeserializeObject<object>(textJSON);
-                System.IO.File.WriteAllText(@"C:\Temp\STEP2.txt", "Posle deserializacije:" + newJSON.ToString());
-                listUser = GetAllUsers(newJSON.ToString());
+                listUser = GetAllUsers(textJSON);
                 int index = listUser.FindIndex(x => x.Email.Contains(usermail));
                 if (index != -1)
                     userID = listUser[index].User_Guid;
+                System.IO.File.WriteAllText(@"C:\Temp\TestGetUserByMail.txt", "Izlaza iz GetUserByMail:" + " trazeni GUID  :" + userID);
             }
             catch (System.Exception e)
             {
@@ -30,21 +29,13 @@ namespace PluginLibrary.Models
             }
             return userID;
         }
-        public static List<User> GetAllUsers(dynamic newJSON)
+        public static List<User> GetAllUsers(string textJSON)
         {
-            System.IO.File.WriteAllText(@"C:\Temp\STEP3.txt", "usao u GetAllUsers:");
             List<User> list = new List<User>();
 
-            //string pathFileUpis = @"C:\Temp\Test\TestVrtimoGUID.txt";
-            //using (StreamWriter sw = File.CreateText(pathFileUpis))
-            //{
-            //    sw.WriteLine($"KRECE UPIS PODATAKA IZ JSON FILE");
-            //    sw.WriteLine("");
-            //}
-            //System.IO.File.WriteAllText(@"C:\Temp\textJSONpreneseni.txt", "STEP 2");
-
-            //dynamic newJSON = JsonConvert.DeserializeObject<object>(textJSON);
+            dynamic newJSON = JsonConvert.DeserializeObject<object>(textJSON);
             int i = 1;
+            System.IO.File.WriteAllText(@"C:\Temp\GetAllUsers.txt", "usao u GetAllUsers newJSON i:" + newJSON.ToString());
             foreach (var item in newJSON.data)
             {
                 list.Add(new User
@@ -54,13 +45,14 @@ namespace PluginLibrary.Models
                     Email = item[2],
                     User_Guid = item[9]
                 });
-                //System.IO.File.WriteAllText(@"C:\Temp\textJSONpreneseni.txt", "STEP:" + i.ToString());
-                //using (StreamWriter sw = File.AppendText(pathFileUpis))
-                //{
-                //    sw.WriteLine($"User name: {item[2].ToString()}  user ID : {item[9].ToString()} .");
-                //}
-
+                using (StreamWriter sw = File.AppendText(@"C:\Temp\GetAllUsers.txt"))
+                {
+                    sw.WriteLine($"/r User name:{item[1].ToString()} {item[2].ToString()}  user ID : {item[9].ToString()} .");
+                    sw.WriteLine($"/r IZ LISTE User name:{list[0].FirstName} {list[0].LastName}  user ID : {list[0].User_Guid} .");
+                }
+                i++;
             }
+            //System.IO.File.WriteAllText(@"C:\Temp\GetAllUsersIZLAZ.txt", "usao u GetAllUsers newJSON i:" + list.ToString());
             return list;
         }
     }
--- PluginLibrary/UserEditExtractPlugin.cs
+++ PluginLibrary/UserEditExtractPlugin.cs
@@ -23,7 +23,7 @@ namespace PluginLibrary
 
         //[DisplayName("UserGUID")]
         //[Description("The GUID of the user added in the previous step:")]
-        public string UserGUID { get; set; }
+        //public string UserGUID { get; set; }
 
         public override void Extract(object sender, ExtractionEventArgs e)
         {
@@ -36,6 +36,7 @@ namespace PluginLibrary
 
                 //e.WebTest.Context.Add("NewUser", "234");
                 //System.IO.File.WriteAllText(@"C:\Temp\TestRESPONSE.txt", "Usao u extraction rule, STEP 1");
+                System.IO.File.WriteAllText(@"C:\Temp\TestExtract.txt", "Usao u EXTRACT metodu i zovem GrabValue(e)");
                 GrabValue(e);
             }
         }
@@ -43,12 +44,13 @@ namespace PluginLibrary
         {
 
             string textJson = e.Response.BodyString;
-            string userMail = e.WebTest.Context["email"].ToString() ;
-            UserGUID = UsersProcessing.GetUserByMail(textJson, UserMail);
+            string userMail = e.WebTest.Context["newUserMail"].ToString() ;
+            System.IO.File.WriteAllText(@"C:\Temp\TestGrabValue.txt", "Usao u GrabValue i pozivam GetUserByMail"+ "JSON kao text:" + textJson + "/r a trazi se mail:" + UserMail);
+            string userGUID = UsersProcessing.GetUserByMail(textJson, UserMail);
+            System.IO.File.WriteAllText(@"C:\Temp\TestGrabValueIzlaz.txt", "Izlaz iz GrabValue i  GetUserByMail vratio:" + userGUID);
 
-            System.IO.File.WriteAllText(@"C:\Temp\TestUserGUID.txt", UserGUID);
 
-            e.WebTest.Context.Add("UserGUID", UserGUID);
+            e.WebTest.Context.Add("UserGUID", userGUID);
             //GrabValue(e, table);
         }
 
